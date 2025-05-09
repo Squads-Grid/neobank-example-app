a541449e1d070e4f9d2071c269407c9fdad1caf8
Janine Wagner <janine@sqds.io>
2025-05-09 14:52:28 +0200

    remove console logs

--- app/api/auth+api.ts
+++ app/api/auth+api.ts
@@ -4,13 +4,11 @@ export async function POST(request: Request) {
     try {
         const body = await request.json();
         const response = await gridClient.authenticate(body);
-        console.log("🚀 ~ POST ~ response:", response)
         return new Response(JSON.stringify(response), {
             status: 200,
             headers: { "Content-Type": "application/json" },
         });
     } catch (error: any) {
-        console.log("🚀 ~ POST ~ error:", error)
         // Pass through the error data
         return new Response(
             JSON.stringify(error),
